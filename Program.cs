378d4fbec2cd9cb6e12236ac2773e370d75daa40
Maks0u <maks0u@proton.me>
2024-10-09 20:02:26 +0200

    create http endpoint

--- Program.cs
+++ Program.cs
@@ -4,6 +4,88 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Windows.Media.Control;
 using Windows.Storage.Streams;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+  static async Task Main(string[] args)
+  {
+    try
+    {
+      var builder = WebApplication.CreateBuilder(args);
+      builder.WebHost.ConfigureKestrel(serverOptions =>
+      {
+        serverOptions.ListenLocalhost(5000);
+      });
+
+      var app = builder.Build();
+
+      app.MapGet("/", () => GetMediaPropertiesJson());
+      app.Run();
+    }
+    catch (Exception e)
+    {
+      Console.Error.WriteLine(e.Message);
+    }
+  }
+
+  static async Task<string> GetMediaPropertiesJson()
+  {
+    MediaInfo mediaInfo = await GetMediaProperties();
+    return JsonSerializer.Serialize(mediaInfo, MediaInfoContext.Default.MediaInfo);
+  }
+
+  static async Task<MediaInfo> GetMediaProperties()
+  {
+    var sessionManager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+    var session = sessionManager.GetCurrentSession();
+
+    if (session == null)
+    {
+      Console.Error.WriteLine("No active media session found");
+      return null;
+    }
+
+    var mediaProperties = await session.TryGetMediaPropertiesAsync();
+    if (mediaProperties == null)
+    {
+      Console.Error.WriteLine("Unable to retrieve media properties");
+      return null;
+    }
+
+    return new MediaInfo
+    {
+      Title = mediaProperties.Title ?? "",
+      Artist = mediaProperties.Artist ?? "",
+      Thumbnail = await GetThumbnailInfoAsync(mediaProperties.Thumbnail),
+    };
+  }
+
+  static async Task<ThumbnailInfo> GetThumbnailInfoAsync(IRandomAccessStreamReference thumbnailRef)
+  {
+    if (thumbnailRef == null)
+    {
+      return null;
+    }
+
+    using (var stream = await thumbnailRef.OpenReadAsync())
+    {
+      var reader = new DataReader(stream);
+      await reader.LoadAsync((uint)stream.Size);
+      byte[] bytes = new byte[stream.Size];
+      reader.ReadBytes(bytes);
+
+      return new ThumbnailInfo
+      {
+        Base64 = Convert.ToBase64String(bytes),
+        MimeType = stream.ContentType,
+      };
+    }
+  }
+}
 
 [JsonSourceGenerationOptions(WriteIndented = false)]
 [JsonSerializable(typeof(MediaInfo))]
@@ -28,78 +110,4 @@ public class ThumbnailInfo
 
   [JsonPropertyName("mimeType")]
   public string MimeType { get; set; }
-}
-
-class Program
-{
-  static async Task Main(string[] args)
-  {
-    try
-    {
-      var gsmtcsm = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
-      var currentSession = gsmtcsm.GetCurrentSession();
-      if (currentSession == null)
-      {
-        Console.Error.WriteLine("No active media session found");
-        return;
-      }
-
-      await PrintMediaProperties(currentSession);
-
-      currentSession.MediaPropertiesChanged += async (sender, e) =>
-      {
-        await PrintMediaProperties(currentSession);
-      };
-
-      Console.ReadKey();
-    }
-    catch (Exception e)
-    {
-      Console.Error.WriteLine(e.Message);
-    }
-  }
-
-  static async Task PrintMediaProperties(GlobalSystemMediaTransportControlsSession session)
-  {
-    var mediaProperties = await session.TryGetMediaPropertiesAsync();
-    if (mediaProperties == null)
-    {
-      Console.Error.WriteLine("Unable to retrieve media properties");
-      return;
-    }
-
-    var mediaInfo = new MediaInfo
-    {
-      Title = mediaProperties.Title ?? "",
-      Artist = mediaProperties.Artist ?? "",
-      Thumbnail = await GetThumbnailInfoAsync(mediaProperties.Thumbnail),
-    };
-
-    string jsonString = JsonSerializer.Serialize(mediaInfo, MediaInfoContext.Default.MediaInfo);
-    Console.WriteLine(jsonString);
-  }
-
-  static async Task<ThumbnailInfo> GetThumbnailInfoAsync(
-      IRandomAccessStreamReference thumbnailRef
-  )
-  {
-    if (thumbnailRef == null)
-    {
-      return null;
-    }
-
-    using (var stream = await thumbnailRef.OpenReadAsync())
-    {
-      var reader = new DataReader(stream);
-      await reader.LoadAsync((uint)stream.Size);
-      byte[] bytes = new byte[stream.Size];
-      reader.ReadBytes(bytes);
-
-      return new ThumbnailInfo
-      {
-        Base64 = Convert.ToBase64String(bytes),
-        MimeType = stream.ContentType,
-      };
-    }
-  }
 }
